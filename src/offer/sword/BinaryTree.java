e79f5f1717b3f9789a73f475d8d4f2084c30360b
刘小勇 <liuxiaoyong@192.168.0.122>
2020-11-30 18:34:58 +0800

    重建二叉树

--- src/offer/sword/BinaryTree.java
+++ src/offer/sword/BinaryTree.java
@@ -1,6 +1,7 @@
 package offer.sword;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.concurrent.ConcurrentLinkedDeque;
@@ -246,6 +247,25 @@ public class BinaryTree {
         }
         return level;
     }
+
+    //根据先序遍历和中序遍历重建二叉树
+    public TreeNode reConstructBinaryTree(int [] pre,int [] in) {
+        if (pre.length==0||in.length==0){
+            return null;
+        }
+        if (pre.length!=in.length){
+            return null;
+        }
+        TreeNode root=new TreeNode(pre[0]);
+        for (int i = 0; i < in.length; i++) {
+            if (pre[0]==in[i]){
+                root.left=reConstructBinaryTree(Arrays.copyOfRange(pre,1,i+1),Arrays.copyOfRange(in,0,i));
+                root.right=reConstructBinaryTree(Arrays.copyOfRange(pre,i+1,pre.length),Arrays.copyOfRange(in,i+1,in.length));
+            }
+        }
+        return root;
+    }
+
 
 
 }
